ed45ca2a4c5e13217f906fd66d87edbd55224faf
interactivetimmy <timsch@cisco.com>
2019-03-02 21:29:54 -0500

    hotfix on navigation

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import injectSheet from 'react-jss';
 
-import styles from './navigation.style.js';
+import styles from './navigation.style';
 
 class Navigation extends Component {
   constructor(props) {
